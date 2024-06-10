18d4bce76f1913009160a83a466d5ddc44822476
Manuel Scholz <manuel.serrano943@gmail.com>
2024-06-10 14:45:08 +0200

    Feat: Limpiar input al añadir un nuevo registro y estilos

--- src/list/list-add-item.js
+++ src/list/list-add-item.js
@@ -1,24 +1,36 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { colors, ui } from "../utils/styles";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { insertItemToListId } from "../utils/storage";
 
 export default function ListAddItem({ getChecklist, list }) {
 
     const [value, setValue] = useState("");
+    const input = useRef();
+
+    function onAdd() {
+        input.current.clear();
+        Keyboard.dismiss();
+        setValue("")
+    }
 
     async function add() {
-        await insertItemToListId(list.id, value, false);
-        getChecklist();
+        if (value.length > 0) {
+            await insertItemToListId(list.id, value, false);
+            getChecklist();
+            onAdd();
+        }
     }
 
     return (
         <View style={styles.addWrapper}>
             <TextInput
-                style={styles.input}
+                ref={input}
+                style={[styles.input, ui.muted]}
                 onChangeText={setValue}
                 value={value}
                 placeholder="Añade un nuevo recurso"
+                placeholderTextColor={"#858585"}
             />
             <TouchableOpacity onPress={add}>
                 <Text style={[ui.h2, ui.black, ui.center]}>+</Text>
@@ -31,11 +43,11 @@ const styles = StyleSheet.create({
     addWrapper: {
         flexDirection: "row",
         justifyContent: "space-between",
-        backgroundColor: "lightgray",
+        backgroundColor: "#e3e3e3",
         width: "85%",
         padding: 8,
         borderRadius: 12,
-        marginTop: 24,
+        marginTop: 16,
         paddingHorizontal: 12,
     },
     input: {
